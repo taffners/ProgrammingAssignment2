6e6ed0fe3afb10cfdd0edf5f1a1443bb2a48d7e4
Taffner <staffner@urmc-sh.rochester.edu>
2020-08-22 13:12:16 -0400

    assignment done

---
+++ cachematrix.R
@@ -0,0 +1,82 @@
+# CacheMatrix will store the inverse of a matrix
+
+
+makeCacheMatrix <- function(x = matrix()) 
+{
+     # This function creates a special "matrix" object that can cache its inverse.
+     inverseMat <- NULL
+     initGlobalVars <- function(y)
+     {
+          # save initial matrix and create
+          x <<- y
+          inverseMat <<- NULL
+     }
+     getInitMatrix <- function()
+     {
+          # return initial matrix 
+          x
+     }
+     StoreInverseMatrix <- function(im)
+     {
+          # set inverse matrix
+          inverseMat <<- im
+     }
+     getInverseMatrix <- function()
+     {
+          # returns inverseMat.  If not see NULL will be returned
+          inverseMat
+     }
+     
+     list(initGlobalVars = initGlobalVars, 
+          getInitMatrix = getInitMatrix,
+          StoreInverseMatrix = StoreInverseMatrix,
+          getInverseMatrix = getInverseMatrix)
+}
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) 
+{
+     # Computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+     # If the inverse has already been calculated (and the matrix has not changed), then 
+     # the cachesolve should retrieve the inverse from the cache.
+     
+     # assumptions: supplied matrix is always invertible
+     
+     ## Return a matrix that is the inverse of 'x'
+     
+     ############################################
+     # check if inverse matrix exists.  If NULL is not returned then cached inverse matrix does exist
+     ############################################
+     inv_matrix <- x$getInverseMatrix()
+     
+     if(!is.null(inv_matrix))
+     {
+          message('Getting cached inverse matrix data')
+          return(inv_matrix)
+     }
+     ############################################
+     # if inverse matrix is not cached.  Cache it and return the inverse matrix
+     ############################################
+     
+     # get init matrix
+     init_M <- x$getInitMatrix()
+     
+     # make inverse matrix and cache it. 
+     # Computing the inverse of a square matrix can be done with the solve function in R.
+     inv_matrix <- solve(init_M, ...)
+     
+     # Cache inverse Matrix in obj
+     x$StoreInverseMatrix(inv_matrix)
+     
+     # return inverse matrix
+     inv_matrix
+}
+
+x = matrix(c(rnorm(3,4),rnorm(3,10)), 2,2)
+x
+
+cacheSolve(makeCacheMatrix(x))
+y = matrix(c(rnorm(3,4),rnorm(3,10)), 2,2)
+y
+cacheSolve(makeCacheMatrix(y))
